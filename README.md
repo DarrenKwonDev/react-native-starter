0e20f2095e04fea662230fd5bce8f351b4fa6c8d
DarrenKwonDev <anatomy1545@gmail.com>
2021-02-28 01:21:13 +0900

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# react-native 0.63.4 typescript  
+
+react-native-cli: 2.0.1  
+react-native: 0.63.4  
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+interface Props {}
+
+const App = ({}: Props) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
